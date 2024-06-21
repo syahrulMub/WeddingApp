d6a39c1c1b1f0c624440deaaddb2afc251683804
syahrulMub <syahrul.mubarrok4@gmail.com>
2024-06-22 00:53:47 +0700

    import tailwind css

--- WeddingApp.Web/ClientApp/src/components/FetchDataFunction.js
+++ WeddingApp.Web/ClientApp/src/components/FetchDataFunction.js
@@ -1,44 +1,59 @@
-import React, { useEffect, useState } from "react";
-import 'devextreme/dist/css/dx.darkviolet.css';
+import React, { useEffect, useState, useCallback } from "react";
+import "devextreme/dist/css/dx.light.css";
 import DataGrid, {
-    Column,
-    DataGridTypes,
-    Grouping,
-    Pager,
-    Paging,
-    SearchPanel,
-    Search
+  Column,
+  DataGridTypes,
+  Grouping,
+  Pager,
+  Paging,
+  SearchPanel,
+  Search,
 } from "devextreme-react/cjs/data-grid";
 
 export default function FetchDataGrid() {
-    const [forecasts, setForecasts] = useState([]);
-    const [loading, setLoading] = useState(true);
+  const [forecasts, setForecasts] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-    useEffect(() =>{
-        populateWeatherData();
-    },[])
+  useEffect(() => {
+    populateWeatherData();
+  }, []);
 
-    async function populateWeatherData(){
-        const response = await fetch('weatherforecast',{
-            method: 'GET'
-        });
-        const data = await response.json();
-        setForecasts(data);
-        setLoading(false);
-    }
-    return (
-        <div>
-            <h1 id="tableLabel">Weather APP</h1>
-            <p>This component demonstrates fetching data from the server.</p>
-            {loading ? 
+  const [collapsed, setCollapsed] = useState(true);
+
+  const onContentReady = useCallback(
+    (e) => {
+      if (collapsed) {
+        e.component.expandRow(["EnviroCare"]);
+        setCollapsed(false);
+      }
+    },
+    [collapsed]
+  );
+
+  async function populateWeatherData() {
+    const response = await fetch("weatherforecast", {
+      method: "GET",
+    });
+    const data = await response.json();
+    setForecasts(data);
+    setLoading(false);
+  }
+  return (
+    <div>
+      <h1 id="tableLabel">Weather APP</h1>
+      <p>This component demonstrates fetching data from the server.</p>
+      {/* {loading ? 
             <p><em>loading ...</em></p>
-            :
-            (
-                <DataGrid
-                    dataSource={forecasts}
-                >
-                </DataGrid>
-            )}
-        </div>
-    );
+            : */}
+
+      <DataGrid
+        dataSource={forecasts}
+        allowColumnReordering={true}
+        rowAlternationEnabled={true}
+        showBorders={true}
+        width="100%"
+        onContentReady={onContentReady}
+      ></DataGrid>
+    </div>
+  );
 }
--- WeddingApp.Web/ClientApp/src/index.js
+++ WeddingApp.Web/ClientApp/src/index.js
@@ -1,3 +1,4 @@
+import './style/App.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import React from 'react';
 import { createRoot } from 'react-dom/client';
